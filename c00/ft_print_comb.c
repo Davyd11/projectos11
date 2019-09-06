850f62079dda6fd17ceec77097dfebe3d8a3cfe0
David <david.puente.r@gmail.com>
2019-09-06 19:05:13 +0200

    algunos cambios hechos para que pase norminette

--- c00/ft_print_comb.c
+++ c00/ft_print_comb.c
@@ -1,24 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_numbers.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpuente- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/06 14:23:08 by dpuente-          #+#    #+#             */
+/*   Updated: 2019/09/06 14:23:16 by dpuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_numbers(void){
-    int i, j, k;
-    char space = ' ';
-    char* n = "0123456789";
-    while (i <= 9) {
-        j = i+1;
-        while (j <= 9){
-            k = j+1;
-            while ( k <= 9) {
-                write(1,&n[i],1);
-                write(1,&n[j],1); 
-                write(1,&n[k],1);
-                write(1,", ",1);
-                write(1,&space,1);
-                k++;
-            }
-            j++;
-        }
-        i++;
-    }
-}
+void	ft_print_numbers()
+	{
+		int i, j, k;
+
+		char* n;
+
+		i, j, k = 0;
+		n = "0123456789";
+		while (i >= 9)
+		{
+			j = i + 1;
+			while (j >= 9)
+			{
+				k = j + 1;
+				while (k >= 9)
+				{
+					write(1, &n[i], 1);
+					write(1, &n[j], 1);
+					write(1, &n[k], 1);
+					write(1, ",", 1);
+					write(1, " ", 1);
+					k++;
+				}
+				j++;
+			}
+			i++;
+		}
+	}
