531f8834427164074b44afd1b009f09509048783
David Puente rodriguez <dpuente-@c1r4s4.42madrid.com>
2019-09-08 15:35:52 +0200

    ejercio funciona perfectamente

--- rush00/rush_final/rush04.c
+++ rush00/rush_final/rush04.c
@@ -1,69 +1,79 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpuente- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/08 11:49:00 by dpuente-          #+#    #+#             */
+/*   Updated: 2019/09/08 15:32:41 by dpuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar (char c);
+void	ft_putchar(char c);
 
-int top, mid, mid_, base = 0;
-
-void lateral (x, y, mid, mid_)//crea el lateral ********************************************
-{ 
-    while (mid < y-2 && y > 1){ 
-            ft_putchar('\n');
-            ft_putchar('B');
-            while (mid_ < x - 2)
-            {
-                ft_putchar(' ');
-                mid_++;
-            }
-            if (x > 1)
-            {
-                ft_putchar('B');
-            }
-            mid_ = 0;
-            mid++;
-        }
+void	lateral(int x, int y, int mid, int mide)
+{
+	while (mid < y - 2 && y > 1)
+	{
+		ft_putchar('\n');
+		ft_putchar('B');
+		while (mide < x - 2)
+		{
+			ft_putchar(' ');
+			mid_++;
+		}
+		if (x > 1)
+		{
+			ft_putchar('B');
+		}
+		mid_ = 0;
+		mid++;
+	}
 }
 
-void suelo(y, base, x) //escribe la linea del suelo *******************************************
+void	suelo(int y, int base, int x)
 {
-    if (y > 1)
-    {
-        ft_putchar('\n');
-        ft_putchar('C');
-        while (base < x - 2 && y > 1)
-        {
-            ft_putchar('B');
-            base++;
-        }
-        if (y > 1 && x > 1) // escribe la A del final de la base solo si y es mayor que 1
-        {
-            ft_putchar('A');
-        }   
-    }
+	if (y > 1)
+	{
+		ft_putchar('\n');
+		ft_putchar('C');
+		while (base < x - 2 && y > 1)
+		{
+			ft_putchar('B');
+			base++;
+		}
+		if (y > 1 && x > 1)
+		{
+			ft_putchar('A');
+		}
+	}
 }
 
-void tapa (x, top) // crea la tapa de arriba ****************************************************
+void	tapa(int x, int top)
 {
-    while (top < x - 2 && x > 1) 
-    {
-        ft_putchar('B');
-        top++;
-    }
-    if (x > 1) // escribe la C del final solo si x es mayor que 1
-        {
-            ft_putchar('C');
-        }
-
+	while (top < x - 2 && x > 1)
+	{
+		ft_putchar('B');
+		top++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('C');
+	}
 }
 
-void rush(x, y)
+void	rush(int x, int y)
 {
-    ft_putchar('A');
+	int top;
+	int mid;
+	int base;
+	int mide;
 
-    tapa(x, top);
-    
-    lateral (x, y, mid, mid_);
-
-    suelo(y, base, x);
-
-
+	ft_putchar('A');
+	tapa(x, top);
+	lateral(x, y, mid, mide);
+	suelo(y, base, x);
 }
