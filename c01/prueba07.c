183aeae00906b31e96b4986caad724a3d3b316f3
David Puente rodriguez <dpuente-@c2r7s3.42madrid.com>
2019-09-12 15:30:09 +0200

    ejercicio 7

--- c01/prueba07.c
+++ c01/prueba07.c
@@ -4,18 +4,24 @@ void    ft_rev_int_tab(int* tab, int size)
 {
     int n;
     int x[size];
+    int cero;
 
+    cero = 0;
     n = size;
     while (n >= 0)
     {
-        printf("%d", &(tab[size]));
-        //x[n] = 
+        x[n - 1] = tab[cero];
+        cero++;
         n--;
         
     }
+    printf("%d", x[0]);
+    printf("%d", x[1]);
+    printf("%d", x[2]);
 }
 
 int main (void)
 {
-    ft_rev_int_tab(123, 3);
+    int c[3] = {1, 2, 3};
+    ft_rev_int_tab(c, 3);
 }
