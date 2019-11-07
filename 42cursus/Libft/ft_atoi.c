c00df2bda141eb4783334f54c58edcda12f487b9
David Puente rodriguez <dpuente-@c3r6s2.42madrid.com>
2019-11-07 17:06:03 +0100

    update from cluster(atoi,makefile,strdup unfinish)

--- 42cursus/Libft/ft_atoi.c
+++ 42cursus/Libft/ft_atoi.c
@@ -4,9 +4,11 @@ int ft_atoi(const char *str)
 {
     int p;
     int n;
+    int nega;
 
     n = 0;
     p = 0;
+    nega = 0;
     while (str[n] != '\0')
     {
         if((str[n] < 58)&&(str[n] > 47))
@@ -14,13 +16,23 @@ int ft_atoi(const char *str)
             p = (p * 10) + (str[n] - '0');
             n++;
         }
+        else if ((str[0] == 45) && ((str[1] != 45) || (str[1] != 43)))
+        {
+            nega++;
+            n++;
+        }
+        else if(str[0] == 43)
+            n++;
         else 
             return(0);
     }
-    return(p);
+    if (nega > 0)
+        return((p)*(-1));
+    else
+        return(p);
 }
 int main ()
 {
-    char src[] = "10";
+    char src[] = "123456789";
     printf("%d",ft_atoi(src));
 }
---
+++ 42cursus/Libft/ft_strdup.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+char *strdup(const char *s)
+{
+    
+}
