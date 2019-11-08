ef2742ebfd28b0a9cb62a03a448f415fa3705062
David <david.puente.r@gmail.com>
2019-11-08 10:38:16 +0100

    strdup done waiting for testing and revesion

--- 42cursus/Libft/ft_strdup.c
+++ 42cursus/Libft/ft_strdup.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
 
-char *strdup(const char *s)
+char	*ft_strdup(char *src)
 {
-    
+	char	*new;
+	int		i;
+	int		size;
+
+	size = 0;
+	while (src[size])
+		++size;
+	if (!(new = malloc(sizeof(char) * (size + 1))))
+		return (NULL);
+	i = 0;
+	while (src[i])
+	{
+		new[i] = src[i];
+		i++;
+	}
+	new[i] = '\0';
+	return (new);
+}
+
+int main()
+{
+	char source[] = "Hola David";
+
+	char* target = ft_strdup(source);
+	printf("%s", target);
 }
