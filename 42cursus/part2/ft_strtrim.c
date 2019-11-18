fb9be7c4dcf7cae0895c4e178c7f795b35e68f64
David <david.puente.r@gmail.com>
2019-11-18 19:24:53 +0100

    strtrim finished for now

--- 42cursus/part2/ft_strtrim.c
+++ 42cursus/part2/ft_strtrim.c
@@ -3,39 +3,55 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davyd11 <davyd11@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 15:23:19 by davyd11           #+#    #+#             */
-/*   Updated: 2019/11/15 13:00:47 by dpuente-         ###   ########.fr       */
+/*   Updated: 2019/11/18 19:23:43 by davyd11          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include "ft_strlen.c"
+#include "libft.h"
+
+void function(unsigned char *strtrim, unsigned char *strefer, unsigned char *finaltext)
+{
+	int j;
+	int n;
+	int p;
+
+	j = 0;
+	n = 0;
+	p = 0;
+	while (strtrim[n] != '\0')
+	{
+		while(strefer[p] != '\0')
+		{
+			if (strefer[p] == strtrim[n])
+				n++;
+			p++;
+		}
+		p = 0;
+		finaltext[j] = strtrim[n];
+		j++;
+		n++;
+	}
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	int n;
-	int p;
-	char *trim;
-	char *constant;
-	
-	constant = s1;
-	p = 0;
-	n = 0;
-	if (!(p = ft_strlen(s1)))
-		return (0);
-	trim = (char*)malloc(p);
-	p = 0;
-	while (constant[n] = '\0')
-	{
-		if(constant[n] == set[])//hay que resolver dudas sobre que contenido hay que eliminar del string
-			n++;
-			p++;
-		trim[p] = constant[n];
-		n++;
-	}
-	
+	unsigned char *strtrim; 
+	unsigned char *strefer;
+	unsigned char *finaltext;
+
+	strtrim = (unsigned char *) s1;
+	strefer = (unsigned char *) set;
+	finaltext = malloc(ft_strlen(strtrim)12 + 1);
+	function(strtrim, strefer, finaltext);
+	return (finaltext);
 }
-// prodrian ponerme a quitar no solp unas letras si no palabras enterasa ?
+
+int main ()
+{
+	printf("%s",ft_strtrim("hola que tal", "que"));
+}
