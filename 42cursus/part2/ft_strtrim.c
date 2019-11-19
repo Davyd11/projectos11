1257fc73853cb91d3f840906b410dcfb8f31aacd
David Puente rodriguez <dpuente-@c3r7s1.42madrid.com>
2019-11-19 10:29:11 +0100

    ft_strtrim passed norm

--- 42cursus/part2/ft_strtrim.c
+++ 42cursus/part2/ft_strtrim.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: davyd11 <davyd11@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 15:23:19 by davyd11           #+#    #+#             */
-/*   Updated: 2019/11/18 19:31:59 by davyd11          ###   ########.fr       */
+/*   Updated: 2019/11/19 10:27:57 by dpuente-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 #include <stdlib.h>
 #include "libft.h"
 
-void function(unsigned char *strtrim, unsigned char *strefer, unsigned char *finaltext)
+void	function(unsigned char *strtrim, unsigned char *strefer
+, unsigned char *finaltext)
 {
 	int j;
 	int n;
@@ -25,7 +26,7 @@ void function(unsigned char *strtrim, unsigned char *strefer, unsigned char *fin
 	p = 0;
 	while (strtrim[n] != '\0')
 	{
-		while(strefer[p] != '\0')
+		while (strefer[p] != '\0')
 		{
 			if (strefer[p] == strtrim[n])
 				n++;
@@ -40,18 +41,13 @@ void function(unsigned char *strtrim, unsigned char *strefer, unsigned char *fin
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	unsigned char *strtrim; 
+	unsigned char *strtrim;
 	unsigned char *strefer;
 	unsigned char *finaltext;
 
-	strtrim = (unsigned char *) s1;
-	strefer = (unsigned char *) set;
+	strtrim = (unsigned char *)s1;
+	strefer = (unsigned char *)set;
 	finaltext = malloc(ft_strlen(strtrim) + 1);
 	function(strtrim, strefer, finaltext);
 	return (finaltext);
-}
-
-int main ()
-{
-	printf("%s",ft_strtrim("hola que tal", "que"));
 }
