fc95990df58e4996ee6e5f47e1fd3c168b85d58b
David <david.puente.r@gmail.com>
2019-12-03 11:50:50 +0100

    almost all the list projects done

---
+++ 42cursus/libft/ft_lstclear.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+void	ft_lstclear(t_list **begin_list)
+{
+	t_list	*list;
+	t_list	*tmp;
+
+	list = *begin_list;
+	tmp = NULL;
+	if (begin_list == NULL || *begin_list == NULL)
+		return ;
+	while (list)
+	{
+		if (list->next)
+			tmp = list->next;
+		else
+			tmp = NULL;
+		free(list);
+		list = tmp;
+	}
+	*begin_list = NULL;
+}
