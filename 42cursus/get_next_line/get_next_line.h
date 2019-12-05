411d57f2bb83b2cff7830b12be73254d0d4e4ce7
David Puente rodriguez <dpuente-@c2r17s1.42madrid.com>
2019-12-05 12:41:20 +0100

    get_next_line started

---
+++ 42cursus/get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 11:06:44 by dpuente-          #+#    #+#             */
+/*   Updated: 2019/12/05 12:40:46 by dpuente-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+#include <unistd.h>
+#include <stdlib.h>
+//# define BUFF_SIZE 4096 //I dont know if i need to use buzz size here or i must set it later
+
+#endif
