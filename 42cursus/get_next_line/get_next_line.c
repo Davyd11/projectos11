a0e5ebd42cbb639e2cd1506e4821c68003ecc0f1
David <david.puente.r@gmail.com>
2019-12-13 16:29:41 +0100

    next line progress

--- 42cursus/get_next_line/get_next_line.c
+++ 42cursus/get_next_line/get_next_line.c
@@ -3,19 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davyd11 <davyd11@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 11:48:21 by dpuente-          #+#    #+#             */
-/*   Updated: 2019/12/05 12:40:48 by dpuente-         ###   ########.fr       */
+/*   Updated: 2019/12/13 16:17:52 by davyd11          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+static t_list good_file(t_list *file, int fd)
+{
+	
+}
+
 int get_next_line(int fd, char **line)
 {
-	while ()
-	{
-		
-	}
+	char buf[BUFF_SIZE + 1];
+
+	if ((fd < 0 || *line == NULL || read(fd, buf, 0) < 0))
+		return(-1);
+	
 }
--- 42cursus/get_next_line/get_next_line.h
+++ 42cursus/get_next_line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: davyd11 <davyd11@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 11:06:44 by dpuente-          #+#    #+#             */
-/*   Updated: 2019/12/05 12:40:46 by dpuente-         ###   ########.fr       */
+/*   Updated: 2019/12/13 16:20:10 by davyd11          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,13 @@
 # define GET_NEXT_LINE
 #include <unistd.h>
 #include <stdlib.h>
-//# define BUFF_SIZE 4096 //I dont know if i need to use buzz size here or i must set it later
+# define BUFF_SIZE 4242
+
+typedef struct	s_list
+{
+	void				*content;
+	struct s_list		*next;
+}				t_list;
+
 
 #endif
