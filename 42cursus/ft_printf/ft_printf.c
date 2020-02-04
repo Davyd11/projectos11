6447dbf49659fb1b67914ac4859f55295b1536c8
David Puente rodriguez <dpuente-@c1r15s1.42madrid.com>
2020-02-04 17:20:30 +0100

    prints without flags

--- 42cursus/ft_printf/ft_printf.c
+++ 42cursus/ft_printf/ft_printf.c
@@ -6,11 +6,46 @@
 /*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 13:17:45 by dpuente-          #+#    #+#             */
-/*   Updated: 2020/02/04 15:20:05 by dpuente-         ###   ########.fr       */
+/*   Updated: 2020/02/04 17:14:21 by dpuente-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int		ft_strlen(const char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+void	ft_putstr(const char *s)
+{
+	while (*s)
+	{
+		write(1, &(*s), 1);
+		s++;
+	}
+}
+
+typedef struct	s_flags
+{
+	va_list		ap;
+	int			len;
+	int			i;
+	int			flag_width;
+	int			width;
+	int			flag_precision;
+	int			precision;
+}				t_flags;
+
+/*void	percent_finder(const char *format, t_flags *f)
+{
+	
+}*/
 
 int		ft_printf(const char *format, ...)
 {
@@ -19,9 +54,10 @@ int		ft_printf(const char *format, ...)
 	f.len = 0;
 	f.i = 0;
 	va_start(f.ap, format);
-	if (ft_strchr(format, '%'))
-		percent_finder(format, &f);
-	else
+	/*if (ft_strchr(format, '%')) 					//go to search  if there is any %
+		percent_finder(format, &f);*/
+	//else
+	if(format)											//just print on screen all text(no flags)
 	{
 		ft_putstr(format);
 		f.len = ft_strlen(format);
@@ -33,10 +69,12 @@ int		ft_printf(const char *format, ...)
 
 int	main(void)
 {
-	char *s = "Mundo";
+	/*char *s = "Mundo";
 	int i = 56;
-	unsigned int x = 0;
+	unsigned int x = 0;*/
 
-		printf("Hola <%10.5s><%7.4i><%x>\n", s, i, x);
-		ft_printf("Hola <%10.5s><%7.4i><%x>\n", s, i, x);
+		//printf("Hola <%10.5s><%7.4i><%x>\n", s, i, x);
+		//ft_printf("Hola <%10.5s><%7.4i><%x>\n", s, i, x);
+		printf("Hola que tal estamos 12345\n");
+		ft_printf("Hola que tal estamos 12345\n");
 }
--- 42cursus/ft_printf/ft_printf.h
+++ 42cursus/ft_printf/ft_printf.h
@@ -6,24 +6,10 @@
 /*   By: dpuente- <dpuente-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/04 14:54:52 by dpuente-          #+#    #+#             */
-/*   Updated: 2020/02/04 15:19:12 by dpuente-         ###   ########.fr       */
+/*   Updated: 2020/02/04 16:27:37 by dpuente-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
 #include <stdarg.h>
-#include "libft.a"
-
-typedef struct	s_flags
-{
-	va_list		ap;
-	int			len;
-	int			i;
-	int			flag_width;
-	int			width;
-	int			flag_precision;
-	int			precision;
-}				t_flags;
-
-
